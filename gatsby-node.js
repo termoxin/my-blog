87cab5c4dbaffddd6e71e58a0e9ade0a7a4c2d6a
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-05-09 22:42:07 +0300

    Create pages programatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,55 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+// /**
+//  * Implement Gatsby's Node APIs in this file.
+//  *
+//  * See: https://www.gatsbyjs.com/docs/node-apis/
+//  */
 
-// You can delete this file if you're not using it
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { data } = await graphql`
+//     query {
+//       allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+//         edges {
+//           node {
+//             frontmatter {
+//               slug
+//               title
+//             }
+//             id
+//           }
+//         }
+//       }
+//     }
+//   `
+
+//   // Create paginated pages for posts
+
+//   const POSTS_PER_PAGE = 3
+
+//   const numPages = Math.ceil(data.allMdx.edges.length / POSTS_PER_PAGE)
+
+//   Array.from({ length: numPages }).forEach((_, i) => {
+//     actions.createPages({
+//       path: i === 0 ? "/" : `/${i + 1}`,
+//       component: require.resolve("./src/templates/allPosts.js"),
+//       context: {
+//         limit: POSTS_PER_PAGE,
+//         skip: i * POSTS_PER_PAGE,
+//         numPages,
+//         currentPage: i + 1,
+//       },
+//     })
+//   })
+
+//   // Create single blog posts
+
+//   data.allMdx.edges.forEach(edge => {
+//     const slug = edge.node.frontmatter.slug
+//     const id = edge.node.id
+
+//     actions.createPages({
+//       path: slug,
+//       component: require.resolve("./src/templates/singlePost.js"),
+//       context: { id },
+//     })
+//   })
+// }
