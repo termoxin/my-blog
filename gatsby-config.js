b3a85855405037711af7f6380018afca3efeaf83
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-05-09 21:51:19 +0300

    Added config for gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,20 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
