3f7b70aeb7db842fde2cd936897f88a1fd1d90cf
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-05-10 10:52:29 +0300

    Added feature image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import * as React from "react"
 
-import { Container } from "../components"
+import { Container, FeatureImage } from "../components"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Container>
     <Seo title="Home" />
-    <h1>Hello, World!</h1>
+    <FeatureImage />
   </Container>
 )
 
