675274ce237c6d889f164defa73acfb02736d6f1
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-05-12 16:51:43 +0300

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,6 +72,27 @@ module.exports = {
     },
     `gatsby-plugin-mdx`,
     "gatsby-plugin-styled-components",
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-8WKX14M0B7", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
