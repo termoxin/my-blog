8dba66ce460207b295778f9ba7915168897ea60f
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-05-13 09:47:31 +0300

    Add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,6 +93,7 @@ module.exports = {
         },
       },
     },
+    "gatsby-plugin-sitemap",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
