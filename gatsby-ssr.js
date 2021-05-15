47d849e61811782f1cba9feae3ea57b41eb800d5
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-05-15 11:05:21 +0300

    Added SSR

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,12 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from "react"
+import DefaultLayout from "./src/layouts/default"
+
+export const wrapRootElement = ({ element }) => (
+  <DefaultLayout>
+    <article>{element}</article>
+  </DefaultLayout>
+)
