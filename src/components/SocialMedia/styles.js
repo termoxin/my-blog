9b6e2802c2c5f32a682b01832acede1377b0fcad
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-06-29 12:58:27 +0300

    Fix icons

--- src/components/SocialMedia/styles.js
+++ src/components/SocialMedia/styles.js
@@ -1,11 +1,16 @@
 import styled from "styled-components"
 
 export const SocialMediaIcon = styled.img`
-  height: 32px;
-  width: 32px;
+  && {
+    height: 32px;
+    width: 32px;
+  }
 `
 
 export const Anchor = styled.a`
-  height: 32px;
-  width: 32px;
+  && {
+    height: 32px;
+    width: 32px;
+    line-height: 32px;
+  }
 `
